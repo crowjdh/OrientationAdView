ea6a10f8160a2024ec26c16df85b32489e67f20d
Chris Jeong <crowjdh@gmail.com>
2015-02-17 20:38:12 +0900

    initial commit

---
+++ src/main/java/com/yooiistudios/orientationadview/OneShotAdView.java
@@ -0,0 +1,85 @@
+package com.yooiistudios.orientationadview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * Created by Dongheyon Jeong in AdViewLoadTest from Yooii Studios Co., LTD. on 15. 2. 17.
+ *
+ * OneShotAdView
+ *  이미 불려진 광고가 있으면 그 광고를 없에지 않고 유지하면서 visibility 만 조정해 보여주는 뷰
+ */
+@SuppressLint("ViewConstructor")
+public class OneShotAdView extends FrameLayout {
+    private AdView mInnerAdView;
+    private String mAdUnitId;
+    private boolean mHasLoaded;
+
+    public OneShotAdView(Context context, String adUnitId) {
+        super(context);
+        mAdUnitId = adUnitId;
+
+        initInnerAdView();
+        addAdView();
+    }
+
+    private void initInnerAdView() {
+        mInnerAdView = makeLoadedAdView(getContext(), mAdUnitId);
+        mInnerAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                mHasLoaded = true;
+            }
+        });
+    }
+
+    private void addAdView() {
+        addView(mInnerAdView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+
+    public void show() {
+        mInnerAdView.setVisibility(View.VISIBLE);
+        if (!mHasLoaded) {
+            loadAd(mInnerAdView);
+        }
+    }
+
+    public void hide() {
+        mInnerAdView.setVisibility(View.GONE);
+    }
+
+    private static AdView makeLoadedAdView(Context context, String adUnitId) {
+        return makeSmartBannerAdView(context, adUnitId);
+    }
+
+    private static AdView makeSmartBannerAdView(Context context, String adUnitId) {
+        AdView adView = new AdView(context);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        adView.setAdUnitId(adUnitId);
+        return adView;
+    }
+
+    private static AdView loadAd(AdView adView) {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        return adView;
+    }
+
+    public void pause() {
+        mInnerAdView.pause();
+    }
+
+    public void resume() {
+        mInnerAdView.resume();
+    }
+}
---
+++ src/main/java/com/yooiistudios/orientationadview/OrientationAdView.java
@@ -0,0 +1,141 @@
+package com.yooiistudios.orientationadview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.IntDef;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Dongheyon Jeong in News-Android-L from Yooii Studios Co., LTD. on 15. 2. 16.
+ *
+ * OrientationAdView
+ *  화면 회전에 따라 다른 광고를 캐싱하는 클래스
+ */
+@SuppressLint("ViewConstructor")
+public class OrientationAdView extends FrameLayout {
+
+    @IntDef({View.VISIBLE, View.INVISIBLE, View.GONE})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Visibility {}
+
+    private OneShotAdView mPortraitAd;
+    private OneShotAdView mLandscapeAd;
+    private String mPortraitAdUnit;
+    private String mLandscapeAdUnit;
+
+    public OrientationAdView(Context context, String portraitAdUnit, String landscapeAdUnit) {
+        super(context);
+        mPortraitAdUnit = portraitAdUnit;
+        mLandscapeAdUnit = landscapeAdUnit;
+        init();
+    }
+
+    private void init() {
+        initAdViews();
+        showAdBasedOnOrientation();
+    }
+
+    private void initAdViews() {
+        createAdViews();
+        addAllAdViews();
+    }
+
+    private void createAdViews() {
+        mPortraitAd = new OneShotAdView(getContext(), mPortraitAdUnit);
+        mLandscapeAd = new OneShotAdView(getContext(), mLandscapeAdUnit);
+    }
+
+    private void addAllAdViews() {
+        addView(mPortraitAd, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        addView(mLandscapeAd, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        showAdBasedOnOrientation();
+    }
+
+    private void showAdBasedOnOrientation() {
+        if (isPortrait()) {
+            showPortraitAdView();
+        } else {
+            showLandscapeAdView();
+        }
+    }
+
+    private void showPortraitAdView() {
+        mPortraitAd.show();
+        mLandscapeAd.hide();
+    }
+
+    private void showLandscapeAdView() {
+        mPortraitAd.hide();
+        mLandscapeAd.show();
+    }
+
+    public void setVisibility(@Visibility int visibility) {
+        if (mPortraitAd != null) {
+            mPortraitAd.setVisibility(visibility);
+        }
+        if (mLandscapeAd != null) {
+            mLandscapeAd.setVisibility(visibility);
+        }
+    }
+
+//    public void applyBottomMarginOnPortrait(int bottomMargin) {
+//        RelativeLayout.LayoutParams adViewLp =
+//                (RelativeLayout.LayoutParams)mPortraitAd.getLayoutParams();
+//        adViewLp.bottomMargin = bottomMargin;
+//    }
+
+    public void pause() {
+        pausePortraitAd();
+        pauseLandscapeAd();
+    }
+
+    public void resume() {
+        resumePortraitAd();
+        resumeLandscapeAd();
+    }
+
+    private void pausePortraitAd() {
+        if (mPortraitAd != null) {
+            mPortraitAd.pause();
+        }
+    }
+
+    private void pauseLandscapeAd() {
+        if (mLandscapeAd != null) {
+            mLandscapeAd.pause();
+        }
+    }
+
+    private void resumePortraitAd() {
+        if (mPortraitAd != null){
+            mPortraitAd.resume();
+        }
+    }
+
+    private void resumeLandscapeAd() {
+        if (mLandscapeAd != null){
+            mLandscapeAd.resume();
+        }
+    }
+
+    private boolean isPortrait() {
+        return getOrientation() == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    private int getOrientation() {
+        return getContext().getResources().getConfiguration().orientation;
+    }
+}
