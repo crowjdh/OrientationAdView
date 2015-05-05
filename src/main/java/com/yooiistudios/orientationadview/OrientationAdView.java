9fefbffa5c6e4e593f09410a6a530030a19fd853
Chris Jeong <crowjdh@gmail.com>
2015-05-05 18:59:14 +0900

    초기화시 자동으로 show 해주던 것 제거

--- src/main/java/com/yooiistudios/orientationadview/OrientationAdView.java
+++ src/main/java/com/yooiistudios/orientationadview/OrientationAdView.java
@@ -39,7 +39,7 @@ public class OrientationAdView extends FrameLayout {
 
     private void init() {
         initAdViews();
-        show();
+//        show();
     }
 
     private void initAdViews() {
